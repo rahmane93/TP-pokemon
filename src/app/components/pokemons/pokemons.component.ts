23b6d81bf49d913a00aaa27dc3b6f84171e61a5a
Abdourahamane BAH <abdourahamane.bah@etudiant.univ-rennes1.fr>
2020-03-01 22:14:20 +0100

    Q13 : Création d'une autre méthode pour récupérer les informations sur un pokémon. Compléter la classe Pokémon afin de recueillir ces informations.

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -13,20 +13,21 @@ export class PokemonsComponent implements OnInit {
   pokemons: Pokemon[];
   constructor(private  pokemonApiService: PokemonServiceService) {
     this.pokemons = [
-      new Pokemon(0, 'Alpha'),
-      new Pokemon(1, 'Beta'),
-      new Pokemon(2, 'Lambda'),
-      new Pokemon(3, 'Gamma'),
-      new Pokemon(4, 'Sygma'),
-      new Pokemon(5, 'Teta'),
-      new Pokemon(6, 'Pi'),
-      new Pokemon(7, 'Rho')
+      new Pokemon(0, 'Alpha',null),
+      new Pokemon(1, 'Beta',null),
+      new Pokemon(2, 'Lambda',null),
+      new Pokemon(3, 'Gamma',null),
+      new Pokemon(4, 'Sygma',null),
+      new Pokemon(5, 'Teta',null),
+      new Pokemon(6, 'Pi',null),
+      new Pokemon(7, 'Rho',null)
     ];
   }
   ngOnInit() {
     this.pokemonApiService.getPokemons().subscribe((data) => {
+      console.log(data);
       data.results.forEach((e, index) => {
-        this.pokemons.push(new Pokemon(index, e.name));
+        this.pokemons.push(new Pokemon(index, e.name, e.url));
       });
     });
   }
--- src/app/model/pokemon.ts
+++ src/app/model/pokemon.ts
@@ -1,9 +1,11 @@
 export class Pokemon {
   id: number;
   name: string;
+  url: string;
 
-  constructor(id, name) {
+  constructor(id, name, url) {
     this.id = id;
     this.name = name;
+    this.url = url;
   }
 }
